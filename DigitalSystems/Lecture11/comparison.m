32a7de9f82df33f9a80f22945e6e122434b01408
Chris Jobling <cpjobling@gmail.com>
2010-01-28 17:06:35 +0000

    Lecture 11 Complete

---
+++ DigitalSystems/Lecture11/comparison.m
@@ -0,0 +1,35 @@
+%% Compare digital equivalents
+ds = tf(5, [1 5])
+bode(ds)
+grid
+
+%% fs = 15 Hz
+Fs = 15;
+Ts = 1/Fs;
+% Hold equivalent
+dsz=c2d(ds, Ts, 'zoh')
+% Tustin equivalent
+dst = c2d(ds, Ts, 'tustin')
+% Matched pole-zero
+dsm = c2d(ds, Ts, 'mpz')
+% Make Bode plot
+bode(ds,dsz,'*',dst,'+',dsm,'o')
+grid
+legend('D(jw)','ZOH','Tustin','MPZ')
+title('Magnitude and phase of discrete equivalents for D(s) = 5/(s+5) - Fs = 15 Hz')
+pause
+
+%% fs = 5 Hz
+Fs = 5;
+Ts = 1/Fs;
+% Hold equivalent
+dsz=c2d(ds, Ts, 'zoh')
+% Tustin equivalent
+dst = c2d(ds, Ts, 'tustin')
+% Matched pole-zero
+dsm = c2d(ds, Ts, 'mpz')
+% Make Bode plot
+bode(ds,dsz,'*',dst,'+',dsm,'o')
+grid
+legend('D(jw)','ZOH','Tustin','MPZ')
+title('Magnitude and phase of discrete equivalents for D(s) = 5/(s+5) - Fs = 5 Hz')
